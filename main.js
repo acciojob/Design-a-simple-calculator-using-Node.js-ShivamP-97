55c22ac9b082ac5b1e692be398ba56f88411e52b
= <shivampathak19972@gmail.com>
2026-01-08 14:48:12 +0530

    Test

--- main.js
+++ main.js
@@ -1,31 +1,78 @@
-// Calculator program in Node.js
+
+const readline = require("readline");
 
 // Function to perform addition
 function add(num1, num2) {
-  // TODO: Perform addition operation here
+  return num1 + num2;
 }
 
 // Function to perform subtraction
 function subtract(num1, num2) {
-  // TODO: Perform subtraction operation here
+  return num1 - num2;
 }
 
 // Function to perform multiplication
 function multiply(num1, num2) {
-  // TODO: Perform multiplication operation here
+  return num1 * num2;
 }
 
 // Function to perform division
 function divide(num1, num2) {
-  // TODO: Perform division operation here
+  if (num2 === 0) {
+    throw new Error("Error: Division by zero");
+  }
+  return num1 / num2;
 }
 
 // Main program
 function calculator() {
-  // TODO: Get user input for numbers and operation
-  // TODO: Call the appropriate function based on the operation
-  // TODO: Output the result
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+
+  rl.question("Enter first number: ", (num1) => {
+    rl.question("Enter second number: ", (num2) => {
+      rl.question(
+        "Enter operation (add, subtract, multiply, divide): ",
+        (operation) => {
+          const a = Number(num1);
+          const b = Number(num2);
+
+          try {
+            let result;
+
+            switch (operation) {
+              case "add":
+                result = add(a, b);
+                break;
+              case "subtract":
+                result = subtract(a, b);
+                break;
+              case "multiply":
+                result = multiply(a, b);
+                break;
+              case "divide":
+                result = divide(a, b);
+                break;
+              default:
+                console.log("Invalid operation");
+                rl.close();
+                return;
+            }
+
+            console.log("Result:", result);
+          } catch (error) {
+            console.log(error.message);
+          }
+
+          rl.close();
+        }
+      );
+    });
+  });
 }
 
-// Invoke the calculator function to start the program
 calculator();
+
+module.exports = { add, subtract, multiply, divide };
